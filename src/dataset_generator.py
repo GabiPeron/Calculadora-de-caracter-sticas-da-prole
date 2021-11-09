5784a5dbd09d7ffa53f5dfa92e0a3f0c7ad2563f
GabiPeron <peronn07@gmail.com>
2021-11-08 23:22:52 -0300

    Simplificando dataset

--- src/dataset_generator.py
+++ src/dataset_generator.py
@@ -5,22 +5,22 @@ import random
 genes = {
     'eyeColor': ['O', 'o'],
     'hairColor': ['C', 'c'],
-    'skinColor': ['P', 'p'],
-    'dominantHand': ['M', 'm'],
-    'earLobule': ['R', 'r'],
-    'noseFormat': ['N', 'n'],
-    'hairSwirl': ['S', 's'],
-    'widowBeak': ['V', 'v'],
-    'dimples': ['A', 'a'],
-    'ptc': ['T', 't'],
-    'thumb': ['L', 'l'],
-    'earTip': ['U', 'u'],
-    'eyebrow': ['B', 'b'],
-    'freckles': ['D', 'd'],
-    'eyelash': ['I', 'i'],
-    'eyeFormat': ['H', 'h'],
-    'cerumen': ['E', 'e'],
-    'wisdowTeeth': ['Z', 'z']
+    # 'skinColor': ['P', 'p'],
+    # 'dominantHand': ['M', 'm'],
+    # 'earLobule': ['R', 'r'],
+    # 'noseFormat': ['N', 'n'],
+    # 'hairSwirl': ['S', 's'],
+    # 'widowBeak': ['V', 'v'],
+    # 'dimples': ['A', 'a'],
+    # 'ptc': ['T', 't'],
+    # 'thumb': ['L', 'l'],
+    # 'earTip': ['U', 'u'],
+    # 'eyebrow': ['B', 'b'],
+    # 'freckles': ['D', 'd'],
+    # 'eyelash': ['I', 'i'],
+    # 'eyeFormat': ['H', 'h'],
+    # 'cerumen': ['E', 'e'],
+    # 'wisdowTeeth': ['Z', 'z']
 }
 
 index = 1
@@ -32,14 +32,14 @@ with open('dataset.csv', 'w', newline='') as dataset:
     writer = csv.DictWriter(dataset, fieldnames=header)
     writer.writeheader()
 
-    while (index <= 10000):
+    while (index <= 2):
         row = {
             'index' : index,
             'name': names.get_first_name()
         }
 
         for gene in genes:
-            row[gene] = genes[gene][random.randint(0, 1)]
+            row[gene] = (genes[gene][random.randint(0, 1)] + genes[gene][random.randint(0, 1)])
 
         writer.writerow(row)
 
