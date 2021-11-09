1215eeec1b45c7c99e046cf2513ff0d0abfaf384
GabiPeron <peronn07@gmail.com>
2021-11-09 00:09:04 -0300

    Gerando punnett

--- src/punnett_square.py
+++ src/punnett_square.py
@@ -1,5 +1,23 @@
 import pandas as pd
+from itertools import groupby, product
+
+def allele(e):
+    return [list(v) for _, v in groupby(e, key = str.lower)]
+
+def punnett(a, b):
+    return [''.join(e)
+        for e in product(*([''.join(e) for e in product(*e)]
+                    for e in zip(allele(a), allele(b))))]
 
 dataset = pd.read_csv('dataset.csv')
 
-print(dataset)
+crossover_samples = dataset.sample(2)
+
+crossover_genotypes = []
+
+for sample in crossover_samples.itertuples():
+    crossover_genotypes.append(sample.eyeColor + sample.hairColor)
+
+# for genotype in crossover_genotypes:
+    # print(set([''.join(p) for p in permutations(crossover_genotypes[genotype])]))
+print(punnett(crossover_genotypes[0], crossover_genotypes[1]))
